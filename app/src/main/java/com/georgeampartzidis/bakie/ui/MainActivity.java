5d5b18c23d3de24240712bb9303151bbb67e6db2
George Ampartzidis <ambageo@gmail.com>
2018-08-06 15:41:50 +0100

    Added Volley gradle dependency and implementation

--- app/src/main/java/com/georgeampartzidis/bakie/ui/MainActivity.java
+++ app/src/main/java/com/georgeampartzidis/bakie/ui/MainActivity.java
@@ -3,10 +3,21 @@ package com.georgeampartzidis.bakie.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.georgeampartzidis.bakie.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String LOG_TAG= MainActivity.class.getSimpleName();
+    public static final String URL="https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
 
     private RecyclerView mRecyclerView;
     @Override
@@ -15,5 +26,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mRecyclerView= findViewById(R.id.rv_recipes);
+
+        sendRecipeRequest();
+    }
+
+    private void sendRecipeRequest(){
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        StringRequest stringRequest= new StringRequest(Request.Method.GET, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getApplicationContext(),
+                                "Response: " + response.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i(LOG_TAG, "Error: " + error.toString());
+            }
+        });
+        requestQueue.add(stringRequest);
     }
 }
