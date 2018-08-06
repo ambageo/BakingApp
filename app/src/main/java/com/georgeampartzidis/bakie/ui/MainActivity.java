f8fa9b94fd9dfb05db5aa91af736a9d02ce0e851
George Ampartzidis <ambageo@gmail.com>
2018-08-06 11:15:24 +0100

    Initial commit

---
+++ app/src/main/java/com/georgeampartzidis/bakie/ui/MainActivity.java
@@ -0,0 +1,19 @@
+package com.georgeampartzidis.bakie.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import com.georgeampartzidis.bakie.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView= findViewById(R.id.rv_recipes);
+    }
+}
