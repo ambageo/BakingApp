519c4075df2b28afd370ec3f4692b100650a6f12
George Ampartzidis <ambageo@gmail.com>
2018-08-17 15:05:59 +0100

    Added Espresso test

--- app/src/androidTest/java/com/georgeampartzidis/bakie/MainActivityRecyclerViewTest.java
+++ app/src/androidTest/java/com/georgeampartzidis/bakie/MainActivityRecyclerViewTest.java
@@ -7,6 +7,7 @@ import android.support.test.runner.AndroidJUnit4;
 
 
 import com.georgeampartzidis.bakie.ui.MainActivity;
+import com.georgeampartzidis.bakie.ui.RecipeActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -14,7 +15,12 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityRecyclerViewTest {
@@ -30,7 +36,10 @@ public class MainActivityRecyclerViewTest {
             e.printStackTrace();
         }
 
-        onView(ViewMatchers.withId(R.id.rv_recipes))
+        onView(withId(R.id.rv_recipes))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
+
+        onView(withId(R.id.toolbar)).check(matches(isDisplayed()));
+        onView(withText("Yellow Cake")).check(matches(withParent(withId(R.id.toolbar))));
     }
 }
